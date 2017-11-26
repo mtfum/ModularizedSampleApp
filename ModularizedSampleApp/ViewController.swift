0f9edf7388ccacd22d12a91ceb0084bd1d30f514
Fumiya Yamanaka <f.yamanaka1127@gmail.com>
2017-11-26 12:57:38 +0900

    Initial Commit

---
+++ ModularizedSampleApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ModularizedSampleApp
+//
+//  Created by Fumiya Yamanaka on 2017/11/26.
+//  Copyright © 2017 mtfum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
